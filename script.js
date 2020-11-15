c0e8d4c08fcfccd8f729bb4b91a862bf8b8766d6
Jamie Munro <74256037+MrRoberson@users.noreply.github.com>
2020-11-15 11:58:34 +0000

    Remove types array, change funFacts to catchphrases

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const fNames = [];
+const lNames = [];
+const types = [];
+const skills = [];
+const weaknesses = [];
+const motivations = [];
+const funFacts = []; 
+
 const characterFactory = (name, type, skills, weakness, funFact) => {
     return {
         name,
